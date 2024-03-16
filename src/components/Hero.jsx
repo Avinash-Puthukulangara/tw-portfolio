803e05e7710535c39f9f8ab0dc31379490e0ed6c
Avinash P <aviputhukulangara@gmail.com>
2024-03-16 19:20:10 +0530

    home page loading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import profileimg from '../assets/prfimg.jpg'
+import { AiFillLinkedin, AiFillGithub, AiFillInstagram } from 'react-icons/ai'
+import {TypeAnimation} from 'react-type-animation'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+        <div className='bg-white max-w-[1200px] h-[83vh] mx-auto flex flex-col-reverse sm:flex-row
+                        justify-center align-center'>
+
+        <div className='text-8xl'>
+            Text
+        </div>
+
+        <div className='my-auto'>
+            <img className='w-[300px] sm:w-[500px] mx-auto h-auto' src={profileimg} alt='Profile Picture'/>
+        </div>
+
+        </div>
+    </div>
   )
 }
 
